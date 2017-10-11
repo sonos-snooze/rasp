a5faee747673df2d7982029a264194360d9c1af8
Fabio <fabio@Fabios-MacBook-Air.local>
2017-10-10 20:24:14 -0400

    added volume increment every second and continuous checking for alarm at the start of every minute. UNTESTED

--- web.py
+++ web.py
@@ -1,6 +1,7 @@
 import requests
 import re
 import datetime
+import time
 
 def alarm_check(time):
          time = re.sub(':','',time)
@@ -10,36 +11,54 @@ def alarm_check(time):
          minute = time /100
          time = time % 100
          sec = time
-         if now.hour == hour:
+         if now.hour == hour:  ##adjust later to actually reflect proper check
                  print('alarm_check true')
                  return 1
          return 0
 
 def play_alarm(songid):
+        basevol = "http://localhost:5005/Living%20Room/volume/"
+        requests.get(basevol+"1")   ##initializes volume to 1
+        
+        ##begins playing song
         base = "http://localhost:5005/Living%20Room/spotify/now/spotify:track:"
-        if songid:
+        if songid: ##null checks songid
                 final = base+songid ##might give error since songid is null
                 print(final);
                 r = requests.get(final)
 
-
-##while 1
-#sleep(x)
-
-
-now = datetime.datetime.now()
-##update to every 5 min
-r = requests.get("http://shrouded-scrubland-66108.herokuapp.com/api/alarms")
-alarms = r.json()
+        for v in range(5,100,5):  ##ups volume
+                vol = str(v)
+                requests.get(basevol+vol) 
+                time.sleep(1)
 
 
 
-#iterate through alarms array
-for a in alarms:
-        if alarm_check(a['alarm_time']):
-                print('calling play_alarm')
-                print(a['spotifySongID'])
-                play_alarm(a['spotifySongID'])
+
+
+while True:
+        now = datetime.datetime.now()
+        print(now)
+        print(now.second)
+        print(now.minute)
+
+        ##update to every 5 min (necessary? possible but not sure if necessary)
+        r = requests.get("http://shrouded-scrubland-66108.herokuapp.com/api/alarms")
+        alarms = r.json()
+
+
+        #iterate through alarms array
+        for a in alarms:
+                if alarm_check(a['alarm_time']):
+                        print('calling play_alarm')
+                        print(a['spotifySongID'])
+                        play_alarm(a['spotifySongID'])
+
+
+        time.sleep(61-now.second)
+
+
+
 
 
 
